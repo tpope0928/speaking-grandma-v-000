cd5350b322dfb08f02c3956f426986738b8e893a
Thomas Pope <tpope0928@gmail.com>
2018-08-06 01:56:03 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,4 @@
 input_phrase = "Hi Nana, how are you?"
 
-def speak_to_grandma(phrase)
-  if phrase == phrase.upcase
-    "NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN"
-  else
-    "HUH?! SPEAK UP, SONNY!"
-  end
-end
+
 
